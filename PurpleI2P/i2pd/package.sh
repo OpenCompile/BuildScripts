5310ff0aacb286a039c2b7a4c16d564c33e7b0a3
JellyCZ <62903038+JellyCZYT@users.noreply.github.com>
2023-09-29 17:35:18 +0000

    Adding deb packaging for i2pd

---
+++ PurpleI2P/i2pd/package.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir -p BuildScripts/PurpleI2P/i2pd/pkg/DEBIAN
+mkdir -p BuildScripts/PurpleI2P/i2pd/pkg/usr/bin
+
+cp Repos/PurpleI2P/i2pd/i2pd BuildScripts/PurpleI2P/i2pd/pkg/usr/bin/.
+
+export PKGARCH=$(dpkg --print-architecture)
+
+touch BuildScripts/$PKGNAME/pkg/DEBIAN/contol
+
+cat <<EOT >> BuildScripts/$PKGNAME/pkg/DEBIAN/contol
+Package: i2pd
+Version: $PKGVER
+Architecture: $PKGARCH
+Essential: no
+Priority: optional
+Depends: libssl-dev;libboost-all-dev
+Maintainer Daniel Hejduk
+Description: I2P: End-to-End encrypted and anonymous Internet
+EOT
+
+dpkg-deb --build BuildScripts/$PKGNAME/pkg/
