274bbd81ea8f127a4e00689f6bebc19aaebf49b5
Daniel Hejduk <jellymail@protonmail.com>
2023-10-13 16:21:48 +0200

    Changing build system

--- PurpleI2P/i2pd/build.sh
+++ PurpleI2P/i2pd/build.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-cd Repos/PurpleI2P/i2pd
+cd Repos/PurpleI2P/i2pd/build
 
-debuild --no-tgz-check -us -uc -b
+cmake -DWITH_STATIC=ON .
 
-cd ../../../
+make
+
+mkdir i2pd_2.49.0
+
+cp i2pd i2pd_2.49.0
+
+tar -czf i2pd_2.49.0.tar.gz i2pd_2.49.0
+
+rm -rf i2pd_2.49.0
+
+cd ../../../../
