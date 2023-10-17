7bd03fd539cd97ad723a57a2bca5497f30410435
Daniel Hejduk <jellymail@protonmail.com>
2023-10-17 18:25:07 +0200

    DEV: Fixing some things

--- xmrig/xmrig/build.sh
+++ xmrig/xmrig/build.sh
@@ -7,7 +7,7 @@ cd build/
 cmake .. -DCMAKE_BUILD_TYPE=Release -DWITH_CN_LITE=OFF -DWITH_CN_HEAVY=OFF -DWITH_CN_PICO=OFF -DWITH_CN_FEMTO=OFF -DWITH_ARGON2=OFF -DWITH_KAWPOW=OFF -DWITH_GHOSTRIDER=OFF
 make -j $(nproc)
 
-mkdir xmrig-opencompile
+mkdir Repos/xmrig/xmrig/build/xmrig-opencompile/
 echo "Some Text" > xmrig-opencompile/test && rm -rf xmrig-opencompile
 
 cp Repos/xmrig/xmrig/build/xmrig Repos/xmrig/xmrig/build/xmrig-opencompile/xmrig
