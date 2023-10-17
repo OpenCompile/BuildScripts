7b5a2fec9ae47c1ded88af1022e5a3798dc39899
Daniel Hejduk <jellymail@protonmail.com>
2023-10-17 18:05:27 +0200

    DEV: Adding Tar compressing

--- xmrig/xmrig/build.sh
+++ xmrig/xmrig/build.sh
@@ -7,4 +7,12 @@ cd build/
 cmake .. -DCMAKE_BUILD_TYPE=Release -DWITH_CN_LITE=OFF -DWITH_CN_HEAVY=OFF -DWITH_CN_PICO=OFF -DWITH_CN_FEMTO=OFF -DWITH_ARGON2=OFF -DWITH_KAWPOW=OFF -DWITH_GHOSTRIDER=OFF
 make -j $(nproc)
 
+mkdir xmrig-opencompile
+echo "Some Text" > xmrig-opencompile/test && rm -rf xmrig-opencompile
+
+cp Repos/xmrig/xmrig/build/xmrig Repos/xmrig/xmrig/build/xmrig-opencompile/xmrig
+tar -czvf Repos/xmrig/xmrig/build/xmrig.tar.gz Repos/xmrig/xmrig/build/xmrig-opencompile
+
+cp Repos/xmrig/xmrig/build/xmrig.tar.gz TarRepo/tmp/xmrig.tar.gz
+
 cd ../../../../
